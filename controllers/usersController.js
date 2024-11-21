801538ceb3dfafe2c008478de121f8c89a3e84af
ernitoft <ernes.fuenzalida@alumnos.ucn.cl>
2024-11-21 16:05:59 -0300

    Obtener un usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,52 @@
+const User = require("../models/database/Users");
 
+const getOneUser = async (req, res) => {
+    try {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader.split(' ')[1];
 
+        if (!token) {
+            return res.status(401).json({
+                error: true,
+                msg: 'No se ha proporcionado un token de autenticación',
+            });
+        }
 
+        const { id } = req.params;
 
-module.exports = {}
+        if (!id) {
+            return res.status(400).json({
+                error: true,
+                msg: 'No se ha proporcionado un ID de usuario',
+            });
+        }
+
+        const user = await User.findByPk(id, {
+            attributes: ['id', 'nombre', 'correoElectronico', 'apellidos', 'createdAt', 'updatedAt', 'estaEliminado'],
+        });
+
+        if (!user || user.estaEliminado) {
+            return res.status(404).json({
+                error: true,
+                msg: 'Usuario no encontrado',
+            });
+        }
+
+        return res.status(200).json({
+            error: false,
+            data: [
+                user,
+            ],
+            msg: 'Usuario encontrado con éxito',
+        });
+
+    } catch (error) {
+        console.log('Error en getOneUser: ', error);
+        return res.status(500).json({
+            error: true,
+            msg: 'Error desconocido obteniendo el usuario: ' + error.message,
+        });
+    }
+}
+
+module.exports = { getOneUser };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,4 +1,7 @@
+const { getOneUser } = require("../controllers/usersController");
 const router = Router();
 
+
+router.get('/{id}', getOneUser);
 
 module.exports = router;
